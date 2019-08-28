59c9352b3af0ff248c938f9500bd284eb4c6056b
Kate H <katehayes.m51@gmail.com>
2019-08-28 12:58:47 -0400

    made signature scraping function

--- src/d02_processing/webscrape.py
+++ src/d02_processing/webscrape.py
@@ -7,9 +7,9 @@ import random
 
 
 
-def forum_topic_fetch(user_agent,):
+def forum_topic_fetch(user_agent):
     """
-      This funciton will fetch the catagories in the NAturally Curly forum page
+      This funciton will fetch the catagories in the Naturally Curly forum page
 
       Keyword arguments:
       user_agent -- Using a different user agent than the default python one keeps the user from being kicked out by the website
@@ -24,45 +24,56 @@ def forum_topic_fetch(user_agent,):
     return categories
 
 
-link_listdiscussion = []
-for i in range(0, 10):
-    url = f'{categories[52]}/p{i}'
-    print(url)
-    user_agent =  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
-    html_page = requests.get(url, headers = {'User-Agent': user_agent} ) #Make a get request to retrieve the page
+def signature_fetch(categories, index_of_category_list, user_agent, start_range=0, finish_range=100):
+    """
+      This funciton will fetch the signatures from a forum topic
 
-    # Check status code for an appropriate response fromt the API
-    status = html_page.status_code
-    if status != 200:
-        print(f'Error improper response code. Code is {status}')
-        #break
-    soup = BeautifulSoup(html_page.content, 'html.parser') #Pass the page contents to beautiful soup for parsing
-    page1_of_discussion = []
-    #time.sleep(random.randint(20,45))
-    for link in soup.find_all('a'):
-        link_listdiscussion.append(link.get('href'))
-        topics = [s for s in link_listdiscussion if (("/discussion" in s) and ('https' in s))]
+      Keyword arguments:
+      catagories -- the list of category urls with no page number on them
+      index_of_category_list -- which url in the list is deisred to be scraped
+      user_agent -- using a different user agent than the default python one keeps the user from being kicked out by the website
+      start_range -- default to start at page zero, but can be set to a different page number as it might need to be run a few times in approximatly 100 page increments
+      finish_range -- default number of pages to stop scraping at, but this might need to be adjusted for very large or small number of pages on each topic url
+    """
+    # Get a list of specific discussion urls
+    link_listdiscussion = []
+    for i in range(start_range, finish_range):
+        url = f'{categories[index_of_category_list]}/p{i}'
+        html_page = requests.get(url, headers = {'User-Agent': user_agent} )
 
-print(len(topics))
-# Loop through all the topics found for each catagory
-list_for_mongo = []
-count = 1
-for topic in topics:
-    url2 = topic
-    user_agent =  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
-    html_page2 = requests.get(url2, headers = {'User-Agent': user_agent} )
+        # Check status code
+        status = html_page.status_code
+        if status != 200:
+            print(f'Error improper response code. Code is {status}')
 
-    if status != 200:
-        print(f'Error improper response code. Code is {status}')
-        #break
+        # Pass the page contents to beautiful soup for parsing
+        soup = BeautifulSoup(html_page.content, 'html.parser')
+        # Create a list of discussions on each forum topic page
+        for link in soup.find_all('a'):
+            link_listdiscussion.append(link.get('href'))
+            topics = [s for s in link_listdiscussion if (("/discussion" in s) and ('https' in s))]
 
-    soup2 = BeautifulSoup(html_page2.content, 'html.parser')
-    signatures = soup2.find_all('div', class_="Signature UserSignature userContent")
+    # Lets the user see the function is working should take a few minutes to get a result here depending on the range of pages looped through
+    print(len(topics))
 
-    for i in range(0,len(signatures)):
-        sig = {}
-        sig['signature'] = soup2.find_all('div', class_="Signature UserSignature userContent")[i].get_text()
-        list_for_mongo.append(sig)
-#     time.sleep(random.randint(1,5))
-    print(count)
-    count += 1
+    # Loop through all the topics found for each catagory
+    list_for_mongo = []
+    count = 1
+    for topic in topics:
+        url2 = topic
+        html_page2 = requests.get(url2, headers = {'User-Agent': user_agent} )
+
+        # Check status code
+        if status != 200:
+            print(f'Error improper response code. Code is {status}')
+
+        soup2 = BeautifulSoup(html_page2.content, 'html.parser')
+        signatures = soup2.find_all('div', class_="Signature UserSignature userContent")
+
+        for i in range(0,len(signatures)):
+            sig = {}
+            sig['signature'] = soup2.find_all('div', class_="Signature UserSignature userContent")[i].get_text()
+            list_for_mongo.append(sig)
+        count += 1
+
+    return list_for_mongo
